1be2ba34521d55acc3e8818a4519702d64c81c88
Ricky Rivas <76985350+RickyRivas@users.noreply.github.com>
2021-10-19 17:22:04 -0500

    Initial commit

---
+++ scripts/main.js
@@ -0,0 +1,115 @@
+// Navigation 1
+const body = document.querySelector('body');
+const burger = document.querySelector(".hamburger");
+const navOverlay = document.querySelector('.navlinks-overlay');
+const nav = document.querySelector(".nav-links");
+
+burger.addEventListener('click', () => {
+    nav.classList.toggle('is-active');
+    burger.classList.toggle('is-active');
+    body.classList.toggle('body-fixed');
+    navOverlay.classList.toggle('is-active');
+})
+navOverlay.addEventListener('click', () => {
+    nav.classList.toggle('is-active');
+    burger.classList.toggle('is-active');
+    body.classList.toggle('body-fixed');
+    navOverlay.classList.toggle('is-active');
+})
+// Color theme toggle
+// const themeToggle = document.querySelector('.theme-toggle');
+// const prefersDarkTheme = window.matchMedia("(prefers-color-scheme: dark)");
+// const currentTheme = localStorage.getItem('theme');
+
+// if (currentTheme == 'dark') {
+//     document.body.classList.toggle('dark-mode')
+// }
+
+// themeToggle.addEventListener('click', () => {
+//     if (prefersDarkTheme.matches) {
+//         document.body.classList.toggle('light-mode')
+//         var theme = document.body.classList.contains('light-mode') ? 'light' : 'dark'
+//     } else {
+//         document.body.classList.toggle('dark-mode');
+//         var theme = document.body.classList.contains('dark-mode') ? 'dark' : 'light'
+//     }
+//     localStorage.setItem('theme', theme)
+// })
+// Reset text 
+// const textInputs = document.querySelectorAll(`.form-control input[type='text']`);
+// textInputs.forEach(input => {
+//     input.addEventListener('focus', () => {
+//         input.value = '';
+//     })
+// })
+var swiper = new Swiper(".landingSwiper", {
+    spaceBetween: 0,
+    centeredSlides: true,
+    loop: true,
+    effect: "fade",
+    autoplay: {
+        delay: 4000,
+        disableOnInteraction: false,
+    },
+    pagination: {
+        el: ".swiper-pagination",
+        clickable: true,
+    },
+    navigation: {
+        nextEl: ".swiper-button-next",
+        prevEl: ".swiper-button-prev",
+    },
+});
+
+
+const tabLinks = document.querySelectorAll('#tab-link');
+const tabContents = document.querySelectorAll('#tabcontent');
+
+tabContents.forEach(tabContent => {
+    if (tabContent.classList.contains('active-content')){
+        tabContent.style.display = 'block';
+     }
+})
+
+const activateTab = (tabLink) => {
+    tabLink.classList.toggle('tabActive');
+}
+
+const deactivateTab = () => {
+    tabLinks.forEach(tabLink => {
+        tabLink.classList.remove('tabActive')
+    })
+}
+
+tabLinks.forEach(tabLink => {
+    tabLink.addEventListener('click', () => {
+        deactivateTab();
+        activateTab(tabLink);
+        showTabContent(tabLink)
+    })
+})
+
+const showTabContent = (tabLink) => {
+    const tabLinkIndex = tabLink;
+    if (tabLinkIndex.classList.contains('tabActive') && tabLinkIndex == tabLinks[0]) {
+        tabContents.forEach(tabContent => {
+            tabContent.classList.remove('active-content')
+        })
+        tabContents[0].classList.add('active-content');
+    } else if (tabLinkIndex.classList.contains('tabActive') && tabLinkIndex == tabLinks[1]) {
+        tabContents.forEach(tabContent => {
+            tabContent.classList.remove('active-content')
+        })
+        tabContents[1].classList.add('active-content')
+    } else if (tabLinkIndex.classList.contains('tabActive') && tabLinkIndex == tabLinks[2]) {
+        tabContents.forEach(tabContent => {
+            tabContent.classList.remove('active-content')
+        })
+        tabContents[2].classList.add('active-content')
+    } else if (tabLinkIndex.classList.contains('tabActive') && tabLinkIndex == tabLinks[3]) {
+         tabContents.forEach(tabContent => {
+            tabContent.classList.remove('active-content')
+        })
+        tabContents[3].classList.add('active-content')
+    }
+}
