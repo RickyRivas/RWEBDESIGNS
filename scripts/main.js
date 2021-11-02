4718a58b67ffdbc3f2176e46da33f7e7675d33ac
Ricky Rivas <76985350+RickyRivas@users.noreply.github.com>
2021-11-01 19:10:12 -0500

    forms temp

--- scripts/main.js
+++ scripts/main.js
@@ -16,25 +16,50 @@ navOverlay.addEventListener('click', () => {
     body.classList.toggle('body-fixed');
     navOverlay.classList.toggle('is-active');
 })
-setTimeout(() => {
-    modalUno.style.display = 'block';
-    body.classList.toggle('body-fixed');
-}, 2000);
-// Popup Modal
+// Tabs
+const tabs = document.querySelectorAll('[data-tab-target]');
+const tabContentSlides = document.querySelectorAll('[data-tab-slide]');
+console.log(tabContentSlides.length)
+
+tabs.forEach(tab => {
+    tab.addEventListener('click', () => {
+        tabs.forEach(tab => tab.classList.remove('tab-active'));
+        tab.classList.add('tab-active')
+        const targets = document.querySelectorAll(tab.dataset.tabTarget);
+        console.log(targets.length)
+        tabContentSlides.forEach(tabSlide => tabSlide.classList.remove('selected'));
+        targets.forEach(target => target.classList.add('selected'));
+    })
+})
+var swiper = new Swiper(".swiper-container-port", {
+    spaceBetween: 10,
+    centeredSlides: true,
+    freeMode: true,
+    pagination: {
+        el: ".swiper-pagination",
+        clickable: true,
+    },
+    navigation: {
+        nextEl: ".swiper-next",
+        prevEl: ".swiper-prev"
+    },
+});
+//Popup Modal
 const modalUno = document.querySelector('.modal-uno-base');
 const modalTriggerUno = document.querySelector('#modal-trigger-uno');
 const closeModalUno = document.querySelector('#close-modal-uno');
 const modalUnoOverlay = document.querySelector('.modal-overlay');
-modalUno.style.display = 'none';
+// modalUno.style.display = 'none';
+body.classList.add('body-fixed');
 
 // modalUnoOverlay.addEventListener('click', () => {
 //     modalUno.style.display = 'none';
 //     body.classList.remove('body-fixed');
 // })
-closeModalUno.addEventListener('click', () => {
-    modalUno.style.display = 'none'
-    body.classList.remove('body-fixed');
-});
+// closeModalUno.addEventListener('click', () => {
+//     modalUno.style.display = 'none'
+//     body.classList.remove('body-fixed');
+// });
 // Color theme toggle
 // const themeToggle = document.querySelector('.theme-toggle');
 // const prefersDarkTheme = window.matchMedia("(prefers-color-scheme: dark)");
@@ -79,84 +104,3 @@ var swiper = new Swiper(".landingSwiper", {
         prevEl: ".swiper-button-prev",
     }
 });
-
-
-const tabLinks = document.querySelectorAll('#tab-link');
-
-const portCards = document.querySelectorAll('.port-card');
-
-const allTab = document.querySelector('.link-1 span');
-const cardsWithAllTag = document.querySelectorAll('.all');
-cardsWithAllTag.forEach(card => {
-    card.style.display = 'block';
-})
-allTab.textContent = `(${cardsWithAllTag.length})`
-
-const landTab = document.querySelector('.link-2 span');
-const cardsWithLandTag = document.querySelectorAll('.lp');
-landTab.textContent = ` (${cardsWithLandTag.length}) `
-
-const fiveTab = document.querySelector('.link-3 span');
-const cardsWithFiveTag = document.querySelectorAll('.five');
-fiveTab.textContent = `(${cardsWithFiveTag.length})`;
-
-const customTab = document.querySelector('.link-4 span');
-const cardsWithCustomTag = document.querySelectorAll('.custom');
-customTab.textContent = `(${cardsWithCustomTag.length})`
-
-const sortItems = () => {
-    tabLinks.forEach(tabLink => {
-
-        tabLink.addEventListener('click', () => {
-
-            const currentlyActiveTab = document.querySelector('.tabActive');
-
-            if (tabLink == currentlyActiveTab) {
-                return;
-            }
-
-            currentlyActiveTab.classList.remove('tabActive');
-
-            tabLink.classList.add('tabActive');
-
-            if (tabLink == tabLinks[0]) {
-                cardsWithAllTag.forEach(card => {
-                    card.style.display = 'block';
-                });
-                portCards.forEach(link => {
-                    if (!link.classList.contains('all')) {
-                        link.style.display = 'none';
-                    }
-                })
-            } else if (tabLink == tabLinks[1]) {
-                cardsWithLandTag.forEach(card => {
-                    card.style.display = 'block';
-                })
-                portCards.forEach(link => {
-                    if (!link.classList.contains('lp')) {
-                        link.style.display = 'none';
-                    }
-                })
-            } else if (tabLink == tabLinks[2]) {
-                cardsWithFiveTag.forEach(card => {
-                    card.style.display = 'block'
-                });
-                portCards.forEach(card => {
-                    if (!card.classList.contains('five')) {
-                        card.style.display = 'none';
-                    }
-                })
-            } else if (tabLink == tabLinks[3]) {
-                cardsWithCustomTag.forEach(card => {
-                    card.style.display = 'block';
-                })
-                portCards.forEach(card => {
-                    if (!card.classList.contains('custom')) {
-                        card.style.display = 'none'
-                    }
-                })
-            }
-        })
-    })
-}
-sortItems();
